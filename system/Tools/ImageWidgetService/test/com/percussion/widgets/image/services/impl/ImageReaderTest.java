27dc8d062cd105a702b60be71d6ad6ad22895d71
Nate Chadwick <nate.chadwick@gmail.com>
2025-12-18 16:28:23 -0500

    Fix failng test.

--- system/Tools/ImageWidgetService/test/com/percussion/widgets/image/services/impl/ImageReaderTest.java
+++ system/Tools/ImageWidgetService/test/com/percussion/widgets/image/services/impl/ImageReaderTest.java
@@ -31,7 +31,7 @@ import org.apache.commons.imaging.Imaging;
 import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.velocity.shaded.commons.io.FilenameUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
