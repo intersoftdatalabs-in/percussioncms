93b0e08ac655c77968500efd5b9b678378d1b8e2
Nate Chadwick <nate.chadwick@gmail.com>
2025-12-28 18:01:28 -0500

    Update jackson configuration to handle change in return of single value objects from arrary to siandalone items.  The serialization issue is from the Jackson 2.14.2 → 2.20.1 upgrade.(.configure(SerializationFeature.WRITE_SINGLE_ELEM_ARRAYS_UNWRAPPED, false))

--- rest/src/main/java/com/percussion/rest/JacksonContextResolver.java
+++ rest/src/main/java/com/percussion/rest/JacksonContextResolver.java
@@ -1,18 +1,17 @@
 /*
  * Copyright 1999-2023 Percussion Software, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied.
  *
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * See the License for the specific language governing permissions and limitations under the
+ * License.
  */
 
 package com.percussion.rest;
@@ -27,9 +26,11 @@ import javax.ws.rs.ext.Provider;
 
 /**
  * @author stephenbolton
- *     <p>This is picked up by Jackson automatically by the Provider annotation It will modify the
- *     serialization behavior of the objects passed in we test that the class has the same ancestor
- *     package as this class to ensure we do not modify behavior for other parts of the system
+ *         <p>
+ *         This is picked up by Jackson automatically by the Provider annotation It will modify the
+ *         serialization behavior of the objects passed in we test that the class has the same
+ *         ancestor package as this class to ensure we do not modify behavior for other parts of the
+ *         system
  */
 @Provider
 @Produces(MediaType.APPLICATION_JSON)
@@ -37,13 +38,13 @@ public class JacksonContextResolver implements ContextResolver<ObjectMapper> {
   private static ObjectMapper objectMapper = new ObjectMapper();
 
   static {
-    objectMapper
-        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true)
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true)
         // Indent may help with testing but can slow performance and can
         // fail unit tests.
         // .configure(SerializationConfig.Feature.INDENT_OUTPUT, true)
         .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
         .configure(SerializationFeature.WRAP_ROOT_VALUE, true)
+        .configure(SerializationFeature.WRITE_SINGLE_ELEM_ARRAYS_UNWRAPPED, false)
         .configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true)
         .configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
   }
@@ -51,11 +52,7 @@ public class JacksonContextResolver implements ContextResolver<ObjectMapper> {
   @Override
   public ObjectMapper getContext(Class<?> objectType) {
     // only use this configuration for classes in same package and subpackages
-    return (objectType
-            .getPackage()
-            .getName()
-            .startsWith(JacksonContextResolver.class.getPackage().getName()))
-        ? objectMapper
-        : null;
+    return (objectType.getPackage().getName()
+        .startsWith(JacksonContextResolver.class.getPackage().getName())) ? objectMapper : null;
   }
 }
