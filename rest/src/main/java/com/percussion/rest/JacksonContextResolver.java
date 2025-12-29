a4ae3498ab27423e159e85439550e889caa511be
Nate Chadwick <nate.chadwick@intsof.com>
2025-12-29 12:23:29 -0500

    Bugfix/510 bad executable bit post install (#515)
    * Fix #510 Add StopJetty.sh script for graceful shutdown of Jetty service
    
    - Implemented a new script to stop the Jetty service, checking for running instances and handling PID files.
    - Added user permission checks to ensure the script runs with the correct user privileges.
    - Included logic for graceful shutdown with a timeout, and forced shutdown if necessary.
    - Enhanced logging to provide feedback during the shutdown process.
    
    * Improved installer instructions for developers
    
    * Remove bad executable bit from StartJetty.sh shebang
    
    * Comment out AxisHTTPSessionListener due to class not found in classpath
    
    * Remove cactus
    
    * Try to close any running instances
    
    * Remove cactus
    
    * Rollback shindig
    
    * Remove defunct repo
    
    * Updates for servlet startup , csrf guard version update.
    
    * Update jackson configuration to handle change in return of single value objects from arrary to siandalone items.  The serialization issue is from the Jackson 2.14.2 → 2.20.1 upgrade.(.configure(SerializationFeature.WRITE_SINGLE_ELEM_ARRAYS_UNWRAPPED, false))
    
    * Fix for  #514 incorrect migration to ITEM_FILE_ATTACHMENT column name on CT_PercFileAsset content type.
    
    * Refactor JacksonContextResolver for improved readability and formatting consistency
    
    * Fix problem where New asset was failing in some scenarios.
    
    * Downgrade PDFBox to 2.0.27
    for compatibility with Java 8.
    
    * Fix randomly failing test

--- rest/src/main/java/com/percussion/rest/JacksonContextResolver.java
+++ rest/src/main/java/com/percussion/rest/JacksonContextResolver.java
@@ -1,18 +1,17 @@
 /*
  * Copyright 1999-2023 Percussion Software, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied.
  *
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * See the License for the specific language governing permissions and limitations under the
+ * License.
  */
 
 package com.percussion.rest;
@@ -44,6 +43,7 @@ public class JacksonContextResolver implements ContextResolver<ObjectMapper> {
         // .configure(SerializationConfig.Feature.INDENT_OUTPUT, true)
         .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
         .configure(SerializationFeature.WRAP_ROOT_VALUE, true)
+        .configure(SerializationFeature.WRITE_SINGLE_ELEM_ARRAYS_UNWRAPPED, false)
         .configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true)
         .configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
   }
