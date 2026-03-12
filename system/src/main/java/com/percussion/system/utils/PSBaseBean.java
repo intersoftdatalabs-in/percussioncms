e5c2d72d0cb5aa2b5f42eaa3c0034da0ce6a1f79
Nate Chadwick <nate.chadwick@intsof.com>
2026-03-12 16:30:44 -0400

    Add AliasFor to clear spring deprecation warning on startup

--- system/src/main/java/com/percussion/system/utils/PSBaseBean.java
+++ system/src/main/java/com/percussion/system/utils/PSBaseBean.java
@@ -21,11 +21,13 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.springframework.core.annotation.AliasFor;
 import org.springframework.stereotype.Component;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Component
 public @interface PSBaseBean {
+  @AliasFor(annotation = Component.class)
   String value() default "";
 }
