0d0df11142c48cfb32059287ac3bfb268faa34b1
Nate Chadwick <263952448+natechadwick-intsof@users.noreply.github.com>
2026-06-19 08:16:53 -0400

    Fix #880 #881: Remove global jQuery UI tooltip that caused tooltips to persist on Home dashboard (#883)
    - Removed .tooltip() initialization that was overriding native browser
      tooltip behavior and causing tooltips to render as persistent page content
    - Removed unused jquery-ui dependency from AMD module definition
    - This fixes the tooltip lifecycle issues where tooltips were not dismissed when
      hovering away or navigating between dashboard tabs (My Recent, My Bookmarks,
      Search Results)
    
    Fixes: #880 Tooltips rendered as persistent page content
    Fixes: #881 Tooltips not dismissed and persist across tabs

--- cui/widgets/contentList/contentList.ViewModel.js
+++ cui/widgets/contentList/contentList.ViewModel.js
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-define(['knockout', 'pubsub', 'utils', 'jquery-ui'], function(ko,PubSub, utils) {
+define(['knockout', 'pubsub', 'utils'], function(ko,PubSub, utils) {
     return function ContentListViewModel(options) {
         var self = this;
         self.options = options;
@@ -371,14 +371,6 @@ define(['knockout', 'pubsub', 'utils', 'jquery-ui'], function(ko,PubSub, utils) 
                 });
             };
         };
-
-        $(function() {
-            $( document ).tooltip({
-                show: {
-                    delay: 1000
-                }
-            });
-        });
 
         //Custom binding for jquery fadeout/fadein as well as inverting the menu if not visible
         ko.bindingHandlers.fadeVisible = {
