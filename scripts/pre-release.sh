9b76dee6af82a0329e570b44f6a22c00e07ee2e4
Nate Chadwick <263952448+natechadwick-intsof@users.noreply.github.com>
2026-06-26 16:41:48 -0400

    fix comand

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -4,16 +4,6 @@ BUILD_ID=1
 BUILD_DATE=$(date -u +%Y%m%d)
 BUILD_STRING="RELEASE"
 
-echo "You can now prepare the release with ./mvn-env.sh mvn release:prepare "
-echo "-DreleaseVersion=8.1.7"
-echo "  -DdevelopmentVersion=8.1.8-SNAPSHOT"
-echo "  -DtagName=v8.1.7"
-echo "  -Darguments=\"-Dmaven.deploy.skip=true\""
-echo "  -DbuildNumber=\"$BUILD_DATE\""
-echo "  -DbuildId=\"$BUILD_ID\""
-echo "  -DversionString=\"RELEASE\""
+echo "You can now prepare the release with ./mvn-env.sh release:prepare -DreleaseVersion=8.1.7 -DdevelopmentVersion=8.1.8-SNAPSHOT -DtagName=v8.1.7 -Darguments=\"-Dmaven.deploy.skip=true\" -DbuildNumber=\"$BUILD_DATE\" -DbuildId=\"$BUILD_ID\" -DversionString=\"RELEASE\""
 
-echo "mvn release:perform -Darguments=\"-Dmaven.deploy.skip=true\""
-echo "  -DbuildNumber=\"$BUILD_DATE\""
-echo "  -DbuildId=\"$BUILD_ID\""
-echo "  -DversionString=\"RELEASE\""
+echo "./mvn-env.sh release:perform -Darguments=\"-Dmaven.deploy.skip=true\" -DbuildNumber=\"$BUILD_DATE\" -DbuildId=\"$BUILD_ID\" -DversionString=\"RELEASE\""
